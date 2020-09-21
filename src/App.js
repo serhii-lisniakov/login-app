a64e776f789a52c9ad1da9c00bca0ff5dd22324a
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-21 22:46:37 +0300

    login-form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <Router basename='/login-app'>
+      <Router basename='/login-form'>
         <Switch>
+          <Route exact path='/' render={() => <Redirect to='/in'/>}/>
           <Route exact path='/in' component={LoginForm}/>
           <Route exact path='/up' component={LoginForm}/>
         </Switch>
