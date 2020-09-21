a8bb5eee767d79da359512022455337eadf3dd72
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-21 22:38:07 +0300

    Updates

---
+++ precache-manifest.4cbc320ded00d29934c58c1b3a60d421.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6ec85266b62428f3d665aa28080b16",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "162493eaf4876b1e5e91",
+    "url": "/login-form/static/css/main.cc7710ac.chunk.css"
+  },
+  {
+    "revision": "7dca889a9510667f5c49",
+    "url": "/login-form/static/js/2.d0e8f1d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-form/static/js/2.d0e8f1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162493eaf4876b1e5e91",
+    "url": "/login-form/static/js/main.01d265ea.chunk.js"
+  },
+  {
+    "revision": "c698525b8492e632f731",
+    "url": "/login-form/static/js/runtime-main.af6c0941.js"
+  },
+  {
+    "revision": "2824b0b15b2d095e3b47343855fc5557",
+    "url": "/login-form/static/media/check.2824b0b1.svg"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/login-form/static/media/padlock.d964e0b3.svg"
+  }
+]);
